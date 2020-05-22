aada6829cddd341d43b7337fe1daa5bbdf3f40c2
RogerPina2 <roger.pina12@hotmail.com>
2020-05-22 09:56:57 -0300

    aula 20

--- aulas/aula20/tarefa-aula20.py
+++ aulas/aula20/tarefa-aula20.py
@@ -29,33 +29,46 @@ T = np.zeros(shape=(ni, nnX, nnY))
 
 T[0][0] = 100
 
+global final
+final = -1
 def metodo_Diferencas_Finitas(T):
     """
         Método das diferenças finitas - bidimensional
         Eq. nos slides da aula 19
     """
+    F0 = (alpha*(deltaT/deltaX**2))
+    convergencia = 0
+
     # Utilizamos l e i para respeitar a equação dos slides
-    for l in range(1, ni):
+    for p in range(1, ni):
         for i in range(nnX):
             for j in range(nnY):
 
-                if i == 0 or j == 0 or i == (nnX-1) or j == (nnY-1):
-                    T[l][i][j] = T[l-1][i][j]
-                else:
-                    T[l][i][j] = T[l-1][i][j] + (alpha*(deltaT/deltaX**2))*((T[l-1][i+1][j] - 2*T[l-1][i][j] + T[l-1][i-1][j]) + (T[l-1][i][j+1] - 2*T[l-1][i][j] + T[l-1][i][j-1]))
+                if j == 0 or i == (nnX-1) or j == (nnY-1):
+                    T[p][i][j] = T[p-1][i][j]
 
+                elif i == 0:
+                    termo_fora_da_placa = 0 #A gente precisa implementar a parte com derivada, só q é dificil, não queremos pensa pq somos preguiçosos :D
+                    T[p][i][j] = F0 * (T[p-1][i+1][j] + termo_fora_da_placa +  T[p-1][i][j+1] + T[p-1][i][j-1]) + (1 - 4*F0)*T[p-1][i][j] #derivada parcial = 0 
+                
+                else:
+                    T[p][i][j] = F0 * (T[p-1][i+1][j] + T[p-1][i-1][j] + T[p-1][i][j+1] + T[p-1][i][j-1]) + (1 - 4*F0)*T[p-1][i][j]
+
+                    if(abs((T[p][i][j]-T[p-1][i][j])/T[p][i][j]) > convergencia):
+                        convergencia = abs((T[p][i][j]-T[p-1][i][j])/T[p][i][j])
+        
+        if(convergencia < 1e-4):
+            final = p
+            break
 
 metodo_Diferencas_Finitas(T)
 
-'''
-plt.plot(T[0])
-plt.show()
-'''
 
 plt.imshow(T[-1])
 plt.colorbar()
 plt.show()
 
 
-print(T[-1])
+print(T[final])
+print(final)
 
