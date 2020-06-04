f559d96e62ca83e2455517599e6839dbff24ede0
RogerPina2 <roger.pina12@hotmail.com>
2020-06-04 19:50:55 -0300

    codigo da ps 4

--- aulas/aula23/aula23.py
+++ aulas/aula23/aula23.py
@@ -1,10 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
 # Lados do retângulo
 Lx = 30
-Ly = 20
+Ly = 30
 
 # Delta X e Delta Y
 dX = 0.5
@@ -15,8 +14,8 @@ nX = int(Lx/dX) + 1
 nY = int(Ly/dY) + 1 
 
 # Tempo total e tempo de despejo
-Tt = 30
-Td = 3
+Tt = 5
+Td = 2
 
 # Intervalo entre cada análise
 dT = 0.05
@@ -31,28 +30,23 @@ C = np.zeros(shape=(nT,nY,nX))
 
 # Valores de K e Q
 K = 1
-Q = 100
+Q = 80
 
 # Valores de u e v
-alpha = 1
-u = alpha
+u = 1
+v = 0
 
 # Valores de a e b
-a = int(8/1.4)
-b = int(60/(8+5))
+a = 15
+b = 15
 
 # Index dos pontos a e b
 idxA = int(nX*a/Lx)
 idxB = int(nY*b/Ly)
 
-final = -1
 for t in range(nT):
-
-    erro = 0
     for y in range(nY):
         for x in range(nX):
-            
-            v = abs(alpha * math.sin((math.pi/5) * x))
 
             if x == 0:
                 C[t][y][x] = C[t][y][x+1]
@@ -71,27 +65,16 @@ for t in range(nT):
                 pU = (u/(2*dX)) * (C[t-1][y][x+1] - C[t-1][y][x-1])
                 pV = (v/(2*dX)) * (C[t-1][y+1][x] - C[t-1][y-1][x])
 
-                if t < nD:
-                    if x == idxA and y == idxB:
-                        pQ = Q/dX**2
-                        C[t][y][x] = ( pQ + pK - pU - pV) * dT + C[t-1][y][x]
+                if t < nD and x == idxA and y == idxB:
+                    pQ = Q/dX**2
+                    C[t][y][x] = ( pQ + pK - pU - pV) * dT + C[t-1][y][x]
                 
                 else:
                     pQ = 0
                     C[t][y][x] = ( pQ + pK - pU - pV) * dT + C[t-1][y][x]
 
-'''
-            if C[t][y][x] != 0:
-                aux = abs((C[t][y][x] - C[t-1][y][x])/(C[t][y][x]))
-                
-                if aux > erro:
-                    erro = aux
+print(C[-1][40][40])
 
-    if erro < tol:
-        final = t
-        break
-'''
-
-plt.imshow(C[final])
+plt.imshow(C[-1])
 plt.colorbar()
 plt.show()
